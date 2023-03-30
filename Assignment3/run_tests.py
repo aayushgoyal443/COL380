50702b66f7f1d793f58bf5eef8afeace2bdfb4ea
aayushgoyal443 <aayushgoyal443@gmail.com>
2023-03-30 22:07:59 +0530

    code from last assignment added

--- Assignment3/run_tests.py
+++ Assignment3/run_tests.py
@@ -1,5 +1,4 @@
 import subprocess
-import time
 
 # for simple commands
 # subprocess.run(["ls", "-l"]) 
@@ -11,8 +10,7 @@ import time
 cmd_str = f"make compile_parallel"
 subprocess.run(cmd_str, shell=True)
 
-# for i in [0,1,2,4,5,6,7,8,3,9,10,11,12]:
-for i in [0,1,2,4,5,6,7,8,3]: # Hi Aayush 
+for i in [0,1,2,4,5,6,7,8,3]:
     # fetch start and end value from our_ass/test_cases/test{i}/info.txt
     # run the test with the given start and end value
     # read the info.txt file
@@ -26,19 +24,12 @@ for i in [0,1,2,4,5,6,7,8,3]: # Hi Aayush
     print(start, end)
     print(f"Compiling test {i}")
     print("Running test " + str(i) + "...")
-    cmd_str = f"make test={i} start={start} end={end} run_parallel_parse_verbose"
+    cmd_str = f"make test={i} start={start} end={end} run_parallel_parse"
     print("Run command: " + cmd_str)
-    start_time = time.time()
     subprocess.run(cmd_str, shell=True)
-    end_time = time.time()
-    print("Time taken: " + str(end_time - start_time) + " seconds")
     print("Done")
     print("Checking results...")
-
-# Hi aayush
-
-
-    cmd_str = f"python3 compare.py results/result_{i}_verbose.txt test_cases/A2/test{i}/output{i}_verbose.txt"
+    cmd_str = f"python3 compare.py results/result_{i}.txt test_cases/A2/test{i}/output{i}.txt"
     print("Comparing using: " + cmd_str)
     subprocess.run(cmd_str, shell=True)
     print("Done")
--- Assignment3/run_tests_verbose.py
+++ Assignment3/run_tests_verbose.py
@@ -12,7 +12,7 @@ cmd_str = f"make compile_parallel"
 subprocess.run(cmd_str, shell=True)
 
 # for i in [0,1,2,4,5,6,7,8,3,9,10,11,12]:
-for i in [0,1,2,4,5,6,7,8,3]: # Hi Aayush 
+for i in [0,1,2,4,5,6,7,8,3]:
     # fetch start and end value from our_ass/test_cases/test{i}/info.txt
     # run the test with the given start and end value
     # read the info.txt file
@@ -34,10 +34,6 @@ for i in [0,1,2,4,5,6,7,8,3]: # Hi Aayush
     print("Time taken: " + str(end_time - start_time) + " seconds")
     print("Done")
     print("Checking results...")
-
-# Hi aayush
-
-
     cmd_str = f"python3 compare.py results/result_{i}_verbose.txt test_cases/A2/test{i}/output{i}_verbose.txt"
     print("Comparing using: " + cmd_str)
     subprocess.run(cmd_str, shell=True)
